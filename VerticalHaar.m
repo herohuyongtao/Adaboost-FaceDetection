95ed70175f2b346fc4fe153a364a5b48fcd00de2
Matthew Farmer <matt@Matthews-MacBook-Pro.local>
2013-03-19 19:59:46 -0500

    Updated Vert

--- VerticalHaar.m
+++ VerticalHaar.m
@@ -2,13 +2,13 @@ fvec = []; %feature vec matrix. PreAllocate for speed when size is known
 W = 19; H=19; %dimensions of training images
 
 % Vertical (Type B) %whitebar, blackbar Vertical 
-for w = 1:floor(W/2)-1 %w=1:8 => 2w=2:16 
-    for h = 1:H-2 
-        for x = 2: H-h %skip first rows 
-            for y = 2:W-2*w %skip first columns 
+for w = 1:W-2  
+    for h = 1:floor(H/2)-1 %h=1:8 => 2h=2:16
+        for x = 2:H-2*h %skip first cols 
+            for y = 2:W-w %skip first columns 
                 feat = zeros(H,W); %initialize to 0 
                 feat(x:x+h-1,y:y+w-1)=1; %white bar 
-                feat(x:x+h-1,y+w:y+2*w-1)=-1; %black bar 
+                feat(x+h:x+2*h-1,y:y+w-1)=-1; %black bar 
                 fvec=[fvec feat(:)']; %vectorize feature 
             end
         end
