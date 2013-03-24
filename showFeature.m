db32d11dd00be4dd52f74a828de2d04d213a5594
Devin <dvillega@gmail.com>
2013-03-24 17:31:35 -0500

    Feature Vec C updated

--- showFeature.m
+++ showFeature.m
@@ -1,10 +1,13 @@
-function showFeature( fvec, index )
+function showFeature( fvec, index)
 %showFeature Utility function to show a feature
 %   Pass in the big feature vector and an index
 %   This will resize the index'd fvec and show it
+%   nxn img
 
 foo = fvec(:,index);
-foo = reshape(foo,19,19);
+n2 = length(foo);
+n = sqrt(n2);
+foo = reshape(foo,n,n);
 imagesc(foo)
 
 
--- threeRectHaar.m
+++ threeRectHaar.m
@@ -1,18 +1,19 @@
-threeRecFvec = []; %feature vec matrix. PreAllocate for speed when size is known
+CFvec = zeros(361,6885); %feature vec matrix. PreAllocated for speed when size is known
 W = 19; H=19; %dimensions of training images
 
-% Horizontal (Type A) %whitebar, blackbar horizontal 
-for w = 1:floor(W/3) %w=1:6 => 2*w=2:12 
+% Three Rec FVec (Type C) %white, black, white horizontal 
+for w = 1:floor(W/3) %w=1:3 => 3*w=3:9 
     for h = 1:H-2 
         for x = 2: H-h %skip first rows 
-            for y = 2:W-2*w %skip first columns 
+            for y = 2:W-3*w %skip first columns 
                 feat = zeros(H,W); %initialize to 0 
-                feat(x:x+h-1,y:y+w-1)=1; %white bar 
-                feat(x:x+h-1,y+w:y+2*w-1)=-1; %black bar 
-                threeRecFvec=[threeRecFvec feat(:)]; %vectorize feature 
+                feat(x:x+h-1,y:y+w-1)=1; %white 1
+                feat(x:x+h-1,y+w:y+2*w-1)=-1; % center black
+                feat(x:x+h-1,y+2*w:y+3*w-1)=1; % white 2
+                CFvec=[CFvec feat(:)]; %vectorize feature 
             end
         end
     end
 end
-%save fvecHoriz.mat threeRecFvec
+save fvecC.mat CFvec
 %clearvars H W h x y
