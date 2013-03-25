c0140731b7396dec6d0a9f56215ccc0a24f2bb5c
Devin Villegas <dvillega@uno.edu>
2013-03-24 19:23:37 -0500

    Tweaked read images to save all variables

--- readImages.m
+++ readImages.m
@@ -4,16 +4,16 @@
 H = 19; W = 19;
 Path = 'train/face/';
 fname = dir('train/face/*.pgm');
-fnum = length(files);
-Imgs = zeros(fnum,W*H);
-cumImgs = zeros(fnum,W*H);
-whiteCumImgs = zeros(fnum,W*H);
+fnum = length(fname);
+faceImgs = zeros(fnum,W*H);
+faceCumImgs = zeros(fnum,W*H);
+faceWhiteCumImgs = zeros(fnum,W*H);
 zeroSD = [];
 % For each file
 for i = 1:fnum
     % Read the image in as a double
     im = im2double(imread([Path,fname(i).name]));
-    cumImgs(i,:) = reshape(cumsum(cumsum(im,2)),1,361);
+    faceCumImgs(i,:) = reshape(cumsum(cumsum(im,2)),1,361);
     imgMean = mean2(im);
     imgSD = std2(im);
     if imgSD < 0.001
@@ -22,14 +22,14 @@ for i = 1:fnum
     
     im = imsubtract(im,imgMean);
     im = im./imgSD;
-    whiteCumImgs(i,:) = reshape(cumsum(cumsum(im,2)),1,361);
-    Imgs(i,:) = im(:)';
+    faceWhiteCumImgs(i,:) = reshape(cumsum(cumsum(im,2)),1,361);
+    faceImgs(i,:) = im(:)';
 end
 
 % Non-Faces
 Path = 'train/non-face/';
 fname = dir('train/non-face/*.pgm');
-fnum = length(files);
+fnum = length(fname);
 nonFaceImgs = zeros(fnum,W*H);
 nonFaceCumImgs = zeros(fnum,W*H);
 nonFaceWhiteCumImgs = zeros(fnum,W*H);
@@ -47,4 +47,5 @@ for i = 1:fnum
     nonFaceImgs(i,:) = im(:)';
 end
 
-clearvars Path H W fname fnum im i imgMean imgSD
+save images.mat nonFaceCumImgs nonFaceImgs nonFaceWhiteCumImgs faceWhiteCumImgs faceImgs faceCumImgs;
+clearvars Path H W fname fnum im i imgMean imgSD;
