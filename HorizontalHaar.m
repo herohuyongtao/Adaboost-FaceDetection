d7573527581c10580c3c8b66daec5a9fee56cb58
Devin <dvillega@gmail.com>
2013-04-03 22:31:08 -0500

    Commented Feature Code - Restructuring Files pt 1 of X

--- HorizontalHaar.m
+++ HorizontalHaar.m
@@ -1,23 +1,32 @@
-haarAFvec = zeros(361,17100); %feature vec matrix. 
-%Preallocate for speed when size is known
-W = 20; H=20; %dimensions of training images
+% This script creates all of the A class Haar features
+% Horizontal Bar 
+%
+%   Sum of [-1's] and [1's]
+%   ____________
+%   |1  1 -1 -1|
+%   |1  1 -1 -1|
+%   """"""""""""
+
+haarAFvec = zeros(361,17100); % feature vec matrix. 
+% Preallocate for speed when size is known
+W = 20; H=20; % dimensions of training images
 
 % Horizontal (Type A) %whitebar, blackbar horizontal 
 count = 0;
-for w = 1:floor(W/2) %w=1:9 => 2w=2:18 
-    for h = 1:H-1 %h=1:18 changed from h-2
-        for x = 1: H-h %original started at 2 skip first rows 
-            for y = 1:W-2*w %originaly started at 2 skip first columns 
+for w = 1:floor(W/2) % w=1:9 => 2w=2:18 
+    for h = 1:H-1 % h=1:18 changed from h-2
+        for x = 1: H-h % original started at 2 skip first rows 
+            for y = 1:W-2*w % originaly started at 2 skip first columns 
                 count = count + 1;
-                feat = zeros(H,W); %initialize to 0 
-                feat(x:x+h-1,y:y+w-1)=1; %white bar 
-                feat(x:x+h-1,y+w:y+2*w-1)=-1; %black bar 
-                 feat(:,end) = [];
+                feat = zeros(H,W); % initialize to 0 
+                feat(x:x+h-1,y:y+w-1)=1; % white bar 
+                feat(x:x+h-1,y+w:y+2*w-1)=-1; % black bar 
+                 feat(:,end) = []; % Delete the padding
                  feat(end,:) = [];
-                haarAFvec(:,count) = feat(:); %vectorize feature 
+                haarAFvec(:,count) = feat(:); % vectorize feature 
             end
         end
     end
 end
-save fvecA.mat haarAFvec
+save fvecA.mat haarAFvec;
 clearvars H W w h x y feat count;
--- VerticalHaar.m
+++ VerticalHaar.m
@@ -1,33 +1,40 @@
-haarBFvec = zeros(361,17100); %feature vec matrix. 
-%Preallocate for speed when size is known
-%clearvars H W h w x y count feat haarBFvec;
-W = 20; H = 20; %dimensions of training images
+% This script creates all of the B class Haar features
+% Vertical Bar 
+%
+%   Sum of [-1's] and [1's]
+%   ____________
+%   | -1 -1 -1 |
+%   | -1 -1 -1 |
+%   | -1 -1 -1 |
+%   |  1  1  1 |
+%   |  1  1  1 |
+%   |  1  1  1 |
+%   """"""""""""
+%
+
+
+haarBFvec = zeros(361,17100); % feature vec matrix. 
+% Preallocate for speed when size is known
+% clearvars H W h w x y count feat haarBFvec;
+W = 20; H = 20; % dimensions of training images + 1 r/c padding
 
 % Vertical (Type B) %whitebar, blackbar Vertical 
 count = 0;
 for w = 1:W-1 % w=1:18 | originaly W-2
-    for h = 1:floor(H/2) %h=1:9 => 2h=2:18 | original floor(H/2)-1
-        for x = 1:H-2*h %skip first rows 
-            for y = 1:W-w %skip first columns 
+    for h = 1:floor(H/2) % h=1:9 => 2h=2:18 | original floor(H/2)-1
+        for x = 1:H-2*h % skip first rows 
+            for y = 1:W-w % skip first columns 
                 count = count + 1;
-                feat = zeros(H,W); %initialize to 0 
+                feat = zeros(H,W); % initialize to 0 
                 feat(x:x+h-1,y:y+w-1)=-1; % black bar
                 feat(x+h:x+2*h-1,y:y+w-1)=1; % white bar
-                feat(:,end) = [];
+                feat(:,end) = []; % Delete the padding
                 feat(end,:) = [];
-                haarBFvec(:,count) =feat(:); %vectorize feature 
+                haarBFvec(:,count) =feat(:); % vectorize feature 
             end
         end
     end
 end
 
-%so we are missing both the column 19 and row 19 in all features
-%this fixes it for the last feature
-% x = x+1; y = y+1;
-% count = count + 1;
-%                 feat = zeros(H,W); %initialize to 0 
-%                 feat(x:x+h-1,y:y+w-1)=-1; % black bar
-%                 feat(x+h:x+2*h-1,y:y+w-1)=1; % white bar
-%                 haarBFvec(:,count) =feat(:); %vectorize feature
-save fvecB.mat haarBFvec
+save fvecB.mat haarBFvec;
 clearvars H W h w x y count feat;
