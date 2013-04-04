74e1e82d9553bbc98b8ba55cfc80d0c0ad7f3d58
Devin <dvillega@gmail.com>
2013-04-04 00:48:39 -0500

    tweeeak!

--- README.md
+++ README.md
@@ -1,4 +1,24 @@
 Group project - CV @ UNO - Spring 2013-
-Ted Holmberg
 Matthew Farmer
 Devin Villegas
+
+HOW TO RUN THIS CODE:
+
+Features are created by the following scripts:
+    A - HorizontalHaar
+    B - VerticalHaar
+    C - ThreeRectHaar
+    D - FourRectHaar
+
+    Reverse Polarity Features are created by fVec.*-1
+
+Images are loaded by:
+    ReadTestImages
+    ReadTrainImages
+    
+Three ways of calculating error:
+    Iteratively - naive functions (These are bad, included for analysis)
+    Viola & Jones vectorized - 
+    Alsamman vectorized - 
+
+
--- checkEndRowsCols.m
+++ checkEndRowsCols.m
@@ -2,27 +2,27 @@ function [ missRow19 missCol19 ] = checkEndRowsCols( fvec )
 %This class was used for testing the haar features to make sure they
 %covered edge rows and columns
 
-%initialized to missed
-missCol19 = 0;
-missRow19 = 0;
+    %initialized to missed
+    missCol19 = 0;
+    missRow19 = 0;
 
-[rows cols] = size(fvec);
-for ind = 1:cols
-foo = fvec(:,ind);
-n2 = length(foo);
-n = sqrt(n2);
-foo = reshape(foo,n,n);
+    [rows cols] = size(fvec);
+    for ind = 1:cols
+        foo = fvec(:,ind);
+        n2 = length(foo);
+        n = sqrt(n2);
+        foo = reshape(foo,n,n);
 
-%if there was a one or a negative one in the row or column, a haar feature
-%used that column or row
-if (max(foo(:,n)) == 1 || min(foo(:,n)) == -1)
-    missCol19 = 1;
-end
-if (max(foo(n,:)) == 1 || min(foo(n,:)) == -1)
-    missRow19 = 1;
-end
-    
-end
+        %if there was a one or a negative one in the row or column, a haar feature
+        %used that column or row
+        if (max(foo(:,n)) == 1 || min(foo(:,n)) == -1)
+            missCol19 = 1;
+        end
+        if (max(foo(n,:)) == 1 || min(foo(n,:)) == -1)
+            missRow19 = 1;
+        end
+            
+    end
 
 end
 
